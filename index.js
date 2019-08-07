bab19cfd4b551be41d915db64801bc16ff4572c6
Amritpal Singh <748914@student.tor.loc>
2019-08-07 17:19:11 -0400

    fi commit

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 var express = require('express');
-var app = express();
-app.get('/',function(req,res){
-    res.send('hello world')
+var app = express()
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+
+
+app.get('/', function(req, res) {
+res.send('helooo there');
 })
+app.post('/', function(req, res) {
+console.log(req.body);
+res.send('post send ');
+});
+app.put('/', function(req, res) {
+
+res.send('this is put');
+});
+app.delete('/', function(req, res) {
+
+res.send('delete');
+})
+
+app.listen(3000);
